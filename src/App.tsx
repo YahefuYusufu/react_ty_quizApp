514d0b39e0f96cad4d7f8a659eb0d529013dd014
prg0615 <parguzatt@gmail.com>
2023-05-24 10:07:10 +0200

    react typescript ruiz app in progress.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { fetchQuestions } from "./API"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//components
+import QuestionCard from "./components/QuestionCard"
+//types
+import { QuestionsState, Difficulty } from "./API"
+
+type AnswerObject = {
+  question: string
+  answer: string
+  correct: boolean
+  correctAnswer: string
 }
 
-export default App;
+const TOTAL_QUESTIONS = 10
+
+const App = () => {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState<QuestionsState[]>([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+
+  console.log(questions)
+
+  const getAPI = async () => {
+    setLoading(true)
+    setGameOver(false)
+
+    const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+
+    setQuestions(newQuestions)
+    setScore(0)
+    setUserAnswers([])
+    setNumber(0)
+    setLoading(false)
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
+  const nextQuestion = () => {}
+
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className="start" onClick={getAPI}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
+      {loading ? <p>Loading Questions...</p> : null}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      <button className="next" onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  )
+}
+
+export default App
